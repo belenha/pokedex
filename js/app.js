bd2447d4078cb4a8c962e8c087cd517c97fad3ed
belenha <lubinatica@gmail.com>
2018-08-02 17:53:27 -0500

    se agregan estilos

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-let minName;
-const pokemonContainer = $("#poke-container");
-let zeros = "00";
-let zero = "0";
+var minName;
+var pokemonContainer = $("#poke-container");
+var zeros = "00";
+var zero = "0";
 for(var i = 1; i < 760; i++) {
     
     if(i < 10) {
-        let picture = $(`<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${zeros}${i}.png">`);
+        var picture = $(`<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${zeros}${i}.png">`);
         // console.log(picture);
         pokemonContainer.append(picture);
     }else if(i <= 99) {
-        let picture = $(`<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${zero}${i}.png">`);
+        var picture = $(`<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${zero}${i}.png">`);
         // console.log(picture);
         pokemonContainer.append(picture); 
     } else {
-        let picture = $(`<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${i}.png">`);
+        var picture = $(`<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${i}.png">`);
         // console.log(picture);
         pokemonContainer.append(picture); 
     }
@@ -22,7 +22,7 @@ for(var i = 1; i < 760; i++) {
 
     $('#pokeBtn').click(function (e) {
         e.preventDefault();
-        const pokemonName = $('#pokeName').val();
+        var pokemonName = $('#pokeName').val();
         minName = pokemonName.toLowerCase();
         getPokemon();
     })
@@ -35,69 +35,69 @@ for(var i = 1; i < 760; i++) {
             crossDomain: true
         }).done(response).fail(error);
     }
-    
+
         function response(data) {
             console.log(data);
-            const name = data.name;
+            var name = data.name;
             console.log(name);
 
-            const url = data.sprites.front_default;
+            var url = data.sprites.front_default;
             console.log(url);
-            const image = "<img src='" + url + "'/>";
+            var image = "<img src='" + url + "'/>";
 
-            const type = data.types[0].type.name;
+            var type = data.types[0].type.name;
             console.log(type);
 
-            const abilities = data.abilities[0].ability.name;
+            var abilities = data.abilities[0].ability.name;
             console.log(abilities);
 
-            const abilitiesTwo = data.abilities[1].ability.name;
+            var abilitiesTwo = data.abilities[1].ability.name;
             console.log(abilitiesTwo);
 
-            const speed = data.stats[0].stat.name + ': ' + data.stats[0].base_stat;
+            var speed = data.stats[0].stat.name + ': ' + data.stats[0].base_stat;
             console.log(speed);
             
-            const specialDefense = data.stats[1].stat.name + ': ' + data.stats[1].base_stat;
+            var specialDefense = data.stats[1].stat.name + ': ' + data.stats[1].base_stat;
             console.log(specialDefense);
             
-            const specialAttack = data.stats[2].stat.name + ': ' + data.stats[2].base_stat;
+            var specialAttack = data.stats[2].stat.name + ': ' + data.stats[2].base_stat;
             console.log(specialAttack);
             
-            const defense = data.stats[3].stat.name + ': ' + data.stats[3].base_stat;
+            var defense = data.stats[3].stat.name + ': ' + data.stats[3].base_stat;
             console.log(defense);
             
-            const attack = data.stats[4].stat.name + ': ' + data.stats[4].base_stat;
+            var attack = data.stats[4].stat.name + ': ' + data.stats[4].base_stat;
             console.log(attack);
             
-            const hp = data.stats[5].stat.name + ': ' + data.stats[5].base_stat;
+            var hp = data.stats[5].stat.name + ': ' + data.stats[5].base_stat;
             console.log(hp);
 
-            const weight = data.weight;
+            var weight = data.weight;
         console.log(weight / 10 + "kg");
 
-        const height = data.height;
+        var height = data.height;
         console.log(height / 10 + "m");
 
-        const identificador = data.id;
+        var identificador = data.id;
         console.log(identificador);
 
             
-        let $pname = $('<p/>').addClass('infoPoke').text(name);
-        let $ptype = $('<p/>').addClass('infoPoke').text(type);
-        let $pabilities = $('<p/>').addClass('infoPoke').text(abilities);
-        let $pabilities2 = $('<p/>').addClass('infoPoke').text(abilitiesTwo);
-        let $pspeed = $('<p/>').addClass('infoPoke').text(speed);
-        let $pspecialDefense = $('<p/>').addClass('infoPoke').text(specialDefense);
-        let $pspecialAttack = $('<p/>').addClass('infoPoke').text(specialAttack);
-        let $pdefense = $('<p/>').addClass('infoPoke').text(defense);
-        let $pattack = $('<p/>').addClass('infoPoke').text(attack);
-        let $php = $('<p/>').addClass('infoPoke').text(hp);
-        let $pweight = $('<p/>').addClass('infoPoke').text(weight);
-        let $pheight = $('<p/>').addClass('infoPoke').text(height);
-        let $pidentificador = $('<p/>').addClass('infoPoke').text(identificador);
+        var $pname = $('<p/>').addClass('infoPoke').text(name);
+        var $ptype = $('<p/>').addClass('infoPoke').text(type);
+        var $pabilities = $('<p/>').addClass('infoPoke').text(abilities);
+        var $pabilities2 = $('<p/>').addClass('infoPoke').text(abilitiesTwo);
+        var $pspeed = $('<p/>').addClass('infoPoke').text(speed);
+        var $pspecialDefense = $('<p/>').addClass('infoPoke').text(specialDefense);
+        var $pspecialAttack = $('<p/>').addClass('infoPoke').text(specialAttack);
+        var $pdefense = $('<p/>').addClass('infoPoke').text(defense);
+        var $pattack = $('<p/>').addClass('infoPoke').text(attack);
+        var $php = $('<p/>').addClass('infoPoke').text(hp);
+        var $pweight = $('<p/>').addClass('infoPoke').text(weight);
+        var $pheight = $('<p/>').addClass('infoPoke').text(height);
+        var $pidentificador = $('<p/>').addClass('infoPoke').text(identificador);
         
 
-       const pokemonContainer = $("#poke-container");
+       var pokemonContainer = $("#poke-container");
         pokemonContainer.append(image, $pname,$ptype,$pabilities,$pabilities2,$pspeed,$pspecialDefense,
                                 $pspecialAttack,$pdefense ,$pattack,$php,$pweight,$pheight,$pidentificador);
         }
